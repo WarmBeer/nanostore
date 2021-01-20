3839fa8eca1315fefabc2e7ad81b469beb801190
Mick <mickvd99@gmail.com>
2021-01-20 22:00:24 +0100

    Added a product page with products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { ProductPageComponent } from './views/product-page/product-page.component';
+import { ProductComponent } from './views/product-page/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    ProductPageComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule
+    RouterModule.forRoot([
+      {path: 'products', component: ProductPageComponent},
+      {path: '', redirectTo: '/products', pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/product-page/product-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-page',
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.css']
+})
+export class ProductPageComponent implements OnInit {
+
+  public products = [
+    {
+      name: 'Haan',
+      price: 20,
+      description: 'Hallo, dit is een test beschrijving.'
+    },
+    {
+      name: 'Kip',
+      price: 10,
+      description: 'Hallo, dit is een test beschrijving.'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
