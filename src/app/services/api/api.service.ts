fd71e11b717ea9628027fcc04178ff5056e52836
Mick <mickvd99@gmail.com>
2021-03-25 17:24:58 +0100

    Auto login with jwt

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {Product} from '../../models/product';
 import {throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
+import {User} from '../../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,11 @@ export class ApiService {
   private API_SERVER = 'http://localhost:80';
   private PRODUCTS_ENDPOINT = '/products';
   private AUTH_ENDPOINT = '/auth';
+  private MY_USER_ENDPOINT = '/myuser';
 
   // tslint:disable:variable-name
-  private _user: {} = {};
-  private _jwt: string;
+  private _user: User;
+  private _jwt = '';
   private _products: Product[] = [];
 
   constructor(private httpClient: HttpClient) {
@@ -25,11 +27,19 @@ export class ApiService {
 
   private initJwt(): void {
     this._jwt = localStorage.getItem(this.JWT_KEY);
-    if (!this._jwt) { this._jwt = ''; }
+    if (!this._jwt) {
+      this._jwt = '';
+    } else {
+      this.loginJwt();
+    }
   }
 
   private saveJwt(): void {
     localStorage.setItem(this.JWT_KEY, this._jwt);
+  }
+
+  private deleteJwt(): void {
+    localStorage.removeItem(this.JWT_KEY);
   }
 
   public get products(): Product[] {
@@ -38,7 +48,6 @@ export class ApiService {
 
   public getProducts(): void {
     this.httpClient.get(this.API_SERVER + this.PRODUCTS_ENDPOINT)
-      .pipe(catchError(this.handleError))
       .subscribe((data: any[]) => {
         this._products = [];
         console.log(data);
@@ -49,8 +58,8 @@ export class ApiService {
       });
   }
 
-  public getUser(): object {
-    return this._user.hasOwnProperty('email') ? this._user : undefined;
+  public getUser(): User {
+    return this._user;
   }
 
   public login(email, password): Promise<any> {
@@ -58,7 +67,7 @@ export class ApiService {
         this.httpClient.post(this.API_SERVER + this.AUTH_ENDPOINT, { email, password })
           .subscribe(
             (data: any) => {
-              this._user = data.user;
+              this._user = new User(data.user.email, data.user.name, data.user.role);
               this._jwt = data.token;
               this.saveJwt();
               resolve('Login success!');
@@ -70,8 +79,24 @@ export class ApiService {
     });
   }
 
-  // tslint:disable-next-line:typedef
-  private handleError(error: HttpErrorResponse) {
-    return throwError(error);
+  private loginJwt(): void {
+    this.httpClient.get(this.API_SERVER + this.MY_USER_ENDPOINT, { headers: new HttpHeaders({Authorization: 'Bearer ' + this._jwt}) })
+      .subscribe(
+        (data: any) => {
+          console.log(data);
+          this._user = data.user;
+        },
+        error => {
+          console.error(error);
+          this.deleteJwt();
+        }
+      );
   }
+
+  public logout(): void {
+    this._user = undefined;
+    this._jwt = '';
+    this.deleteJwt();
+  }
+
 }
